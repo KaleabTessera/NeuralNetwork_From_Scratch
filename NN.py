38837eaf0bb58c82263b95c76bf3c9892c6b70b2
Kale-ab <kaleabtessera@gmail.com>
2019-01-17 18:30:09 +0200

    Implemented feedforward and loss function.

--- NN.py
+++ NN.py
@@ -6,7 +6,6 @@
 # Imports
 import numpy as np
  
- 
 # NN class
 class NN:
     def __init__(self,X,numHiddenLayers,numOfNodesPerHiddenLayer,y,showShapeOfNN=False):
@@ -17,21 +16,47 @@ class NN:
 
         self.w1 =  np.random.rand(self.X.shape[1],numOfNodesPerHiddenLayer[0])
         self.w2 =  np.random.rand(numOfNodesPerHiddenLayer[0],numOfNodesPerHiddenLayer[1])
-        self.w3 =  np.random.rand(numOfNodesPerHiddenLayer[1],y.shape[0])
+        self.w3 =  np.random.rand(numOfNodesPerHiddenLayer[1],y.shape[1])
 
         if(showShapeOfNN):
-            print(self.w1.shape)
-            print(self.w2.shape)
-            print(self.w3.shape)
+            print("X",self.X.shape)
+            print("w1",self.w1.shape)
+            print("w2",self.w2.shape)
+            print("w3",self.w3.shape)
+            print("y",self.y.shape)
+
+    def sigmoid(self,x):
+        return 1 / (1 + np.exp(-x))
+
+    def forward(self):
+        b1 = 1
+        b2 = 1
+        b3 = 1
+
+        # Hidden layers
+        h1 = self.sigmoid(np.dot(self.X,self.w1) + b1)
+        h2 = self.sigmoid(np.dot(h1,self.w2)     + b2)
+        
+        #Output
+        self.pred =  np.dot(h2,self.w3)     + b3
  
- 
+
+    def loss(self):
+        # L2 Norm
+        self.error = np.sum(self.y - self.pred) ** 2
+        print(self.error)
+
+
 def main():
     X = np.ones((100,4), dtype=np.int16 )
-    y = np.random.rand(2)
+    y = np.random.rand(100,2)
     
     numHiddenLayers = 2
     numNodesPerLayer = [10,10]
     neural_network = NN(X,numHiddenLayers,numNodesPerLayer,y)
+
+    neural_network.forward()
+    neural_network.loss()
 
 if __name__ == "__main__":
 	main()
