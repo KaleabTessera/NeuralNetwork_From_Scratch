1f1fb15a2e45c428786337d88695c255048e82f9
Kale-ab <kaleabtessera@gmail.com>
2019-01-18 08:08:57 +0200

    First attempt at backprob alg

--- NN.py
+++ NN.py
@@ -34,16 +34,39 @@ class NN:
         b3 = 1
 
         # Hidden layers
-        h1 = self.sigmoid(np.dot(self.X,self.w1) + b1)
-        h2 = self.sigmoid(np.dot(h1,self.w2)     + b2)
+        self.h1 = self.sigmoid(np.dot(self.X,self.w1) + b1)
+        self.h2 = self.sigmoid(np.dot(self.h1,self.w2)     + b2)
         
         #Output
-        self.pred = self.sigmoid(np.dot(h2,self.w3)     + b3)
+        self.pred = self.sigmoid(np.dot(self.h2,self.w3)     + b3)
  
 
     def loss(self):
         self.error= (-self.y * np.log(self.pred) - (1 - self.y) * np.log(1 - self.pred)).mean()
         print("Loss: ",self.error)
+
+    def sigmoid_der(self,a):
+        return np.dot(self.sigmoid(a),(1 - self.sigmoid(a)).T)
+
+    def backprop(self):
+        # dJQ_dW3 - affect of Weight Layer 3 on cost
+        dS1_dW3 = self.h2
+        dS1 = np.dot(self.h2,self.w3)
+        dy_DS1 = np.dot(self.sigmoid(dS1), (1 - self.sigmoid(dS1)).T)
+        dJQ_dy = (self.pred - self.y )/(self.pred)*(1-self.pred)
+        dJQ_dW3 = dJQ_dy.T.dot(dy_DS1).dot(dS1_dW3)
+        print("dJQ_dW3")
+        print(dJQ_dW3)
+
+        # dJQ_dW2 - affect of Weight Layer 2 on cost
+        dS2_dW2 = self.h1
+        dS2 = np.dot(self.h1,self.w2)
+        dh2_dS2 = np.dot(self.sigmoid(dS2),(1-self.sigmoid(dS2)).T)
+        dJQ_dh2 = self.sigmoid_der(dS1).dot(dJQ_dy).dot(self.w3.T)
+        dJQ_dW2 = dJQ_dh2.T.dot(dh2_dS2).dot(dS2_dW2)
+        print("dJQ_dW2")
+        print(dJQ_dW2)
+
 
 def main():
     X = np.ones((100,4), dtype=np.int16 )
@@ -55,6 +78,7 @@ def main():
 
     neural_network.forward()
     neural_network.loss()
+    neural_network.backprop()
 
 if __name__ == "__main__":
 	main()
