e9dbd195b4e5b88edd9af04572efe5ae74b104c9
Kale-ab <kaleabtessera@gmail.com>
2019-02-01 21:03:48 +0200

    Updated backprop.

--- NN.py
+++ NN.py
@@ -28,8 +28,6 @@ class NN:
             print("y",self.y.shape)
 
     def sigmoid(self,x):
-        # print(x)
-        # return .5 * (1 + np.tanh(.5 * x))
         return 1 / (1 + np.exp(-x))
 
     def forward(self):
@@ -43,7 +41,7 @@ class NN:
         
         #Output
         self.pred = self.sigmoid(np.dot(self.h2,self.w3)     + b3)
-        # print("Predicted: ",self.pred)
+    
     # Mean Squared Error
     def loss(self):
         self.error =  np.mean((self.y - self.pred)**2)
@@ -53,12 +51,9 @@ class NN:
         return 2 *(self.y - self.pred)
       
     def sigmoid_der(self,a):
-        # return np.dot(self.sigmoid(a),(1 - self.sigmoid(a)).T)
         return a * (1.0 - a) 
-        # return (1 / (1 + np.exp(-a)))
 
     def backprop(self,learning_rate=0.1):
-        # print("testtttttttttt")
         # dJQ_dW3 - affect of Weight Layer 3 on cost
         dS3_dW3 = self.h2
         dS3 = np.dot(self.h2,self.w3)
@@ -74,12 +69,8 @@ class NN:
         dh2_dS2 = self.sigmoid_der(dS2)#np.dot(self.sigmoid(dS2),(1-self.sigmoid(dS2)).T)
         
         dS3_dh2 = self.w3
-#         dJQ_dh2 = self.sigmoid_der(dS3).dot(dJQ_dy).dot(self.w3.T)
-#         dJQ_dh2 = dJQ_dy.T.dot(dy_DS3).dot(dS3_dh2) #Check Shape
-        dJQ_dh2 = dS3_dh2.dot(dJQ_dy.T).dot(dy_DS3) #Check Shape
-        dJQ_dW2 = dJQ_dh2.T.dot(np.multiply(dh2_dS2,dS2_dW2).T)
-#         print("dJQ_dW2")
-#         print(dJQ_dW2)
+        dJQ_dh2 = dS3_dh2.dot(np.multiply(dJQ_dy,dy_DS3).T)
+        dJQ_dW2 = dJQ_dh2.dot(np.multiply(dh2_dS2,dS2_dW2)) #TODO Consider which should be multiplied and which dot product.
         
         #dJQ_dW1 - affect of Weight Layer 1 on cost
         dS1_dW1 = self.X
@@ -90,12 +81,8 @@ class NN:
         dh2_dS2 = self.sigmoid_der(dS2)
         dS3_dh2 = self.w3
         
-        dJQ_dh1 = dJQ_dh2.T.dot(dS2_dh1).dot(dh2_dS2.T)
-        
-#         dJQ_dh1 = dh2_dS2.dot(dJQ_dh2).dot(self.w2)
-        dJQ_dW1 =dJQ_dh1.T.dot(dh1_dS1).dot(dS1_dW1)
-#         print("dJQ_dW1")
-#         print(dJQ_dW1)
+        dJQ_dh1 = np.multiply(dJQ_dh2.T,dh2_dS2).dot(dS2_dh1)  #TODO check
+        dJQ_dW1 = np.multiply(dJQ_dh1,dh1_dS1).T.dot(dS1_dW1) #TODO check
         
         self.w1 -= learning_rate * dJQ_dW1.T
         self.w2 -= learning_rate * dJQ_dW2.T
@@ -105,37 +92,19 @@ class NN:
 
         
     def run(self):
-      for i in np.arange(10000):
+      for i in np.arange(10):
         self.forward()
-        # self.loss()
-        self.error =  np.mean((self.y - self.pred)**2)
-        if((i % 10) == 0):
-            print("Loss: ",self.error)
+        self.loss()
         self.backprop()
 
 
 def main():
-    # X = np.ones((100,4), dtype=np.int16 )
-    # y = np.random.rand(100,1)
-
     data = np.loadtxt("data_banknote_authentication.txt",delimiter=',')
     X = data[:,0:4]
-    # np.mean(X) # calculates the mean of the array x
-    # X-np.mean(X) # this is euivalent to subtracting the mean of x from each value in x
-    # X-=np.mean(X) # the -= means can be read as x = x- np.mean(x)
-
-    # np.std(X) # this calcualtes the standard deviation of the array
-    # X/=np.std(X)
-
     
-    # # Get column names first
-    # # names = df.columns
-    # # Create the Scaler object
     scaler = preprocessing.StandardScaler() 
     X = scaler.fit_transform(X)
-    # print(X)
-
-    # X = X / np.sqrt((np.sum(X**2)))
+    
     y = data[:,4].reshape(99,1)
     numHiddenLayers = 2
     numNodesPerLayer = [10,10]
